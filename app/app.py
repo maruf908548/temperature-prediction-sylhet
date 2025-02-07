097727e657309e527bdf4e764ce058831dc255e9
maruf908548 <ahmedmaruf908548@gmail.com>
2025-02-08 01:39:22 +0600

    pkl file link added

--- app/app.py
+++ app/app.py
@@ -8,6 +8,8 @@ st.set_page_config(page_title="Temperature Prediction(Sylhet)", layout="wide")
 
 # Background Image (Replace with your image URL)
 background_image = "https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+
+
 st.markdown(
     f"""
     <style>
